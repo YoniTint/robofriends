276f8dff18392e356c8cdaafe7ab76928d97b18a
YoniTint <yonitint1@gmail.com>
2023-12-15 11:11:43 +0200

    added redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider, connect } from "react-redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { createLogger } from "redux-logger";
+import { thunk } from "redux-thunk";
 import "./index.css";
 import App from "./containers/App";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
+import { searchRobots, requestRobots } from "./reducers";
+
+const logger = createLogger();
+const rootReducer = combineReducers({
+    searchRobots,
+    requestRobots,
+});
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
 
