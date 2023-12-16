46368d335f39a131bdd28c149e6ef6967507d9e5
YoniTint <yonitint1@gmail.com>
2023-12-16 13:41:24 +0200

    optimize

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, combineReducers } from "redux";
-import { createLogger } from "redux-logger";
+//import { createLogger } from "redux-logger";
 import { thunk } from "redux-thunk";
 import "./index.css";
 import App from "./containers/App";
@@ -11,12 +11,11 @@ import "tachyons";
 import * as serviceWorker from "./serviceWorkerRegistration";
 import { searchRobots, requestRobots } from "./reducers";
 
-const logger = createLogger();
 const rootReducer = combineReducers({
     searchRobots,
     requestRobots,
 });
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
